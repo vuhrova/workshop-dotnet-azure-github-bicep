c5bf3504de725065dbbb51ec0da2afb6c34aa035
Veronika Kubikova <vuhrova@microsoft.com>
2024-11-18 14:42:35 -0500

    unhealthy test

--- src/WorkshopDemo/HealthChecks/WorkshopDemoHealthCheck.cs
+++ src/WorkshopDemo/HealthChecks/WorkshopDemoHealthCheck.cs
@@ -6,6 +6,7 @@ public class WorkshopDemoHealthCheck : IHealthCheck
 {
     public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
     {
-        return Task.FromResult(HealthCheckResult.Healthy("A healthy result."));
-    }
+		//return Task.FromResult(HealthCheckResult.Healthy("A healthy result."));
+		return Task.FromResult(HealthCheckResult.Unhealthy("A failure occurred."));
+	}
 }
